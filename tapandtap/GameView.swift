24295a56d38ed16e85fe185254a3908ad7b5b28b
미니 <cow970814@naver.com>
2023-10-06 03:11:30 +0900

    feat : 게임 시작 기능 추가

--- tapandtap/GameView.swift
+++ tapandtap/GameView.swift
@@ -8,13 +8,16 @@ import SwiftUI
 
 struct GameView: View {
     @State private var userCount: Int = 2
+    @State private var isStart: Bool = false
+    @State private var targetNumber: Int = .zero
     
     var body: some View {
-        ZStack {
-            Color.black.edgesIgnoringSafeArea(.all)
-            
+        VStack {
             GameUserControl(userCount: $userCount)
                 .padding(.horizontal, 16)
+                .disabled(isStart)
+            
+            Spacer()
             
             LazyVGrid(
                 columns: Array(
@@ -22,10 +25,40 @@ struct GameView: View {
                     count: 3
                 )
             ) {
-                ForEach(0..<userCount, id: \.self) { _ in
-                    
+                ForEach(1...userCount, id: \.self) { index in
+                    Button {
+                        print("Tapped Card")
+                    } label: {
+                        Image(index == targetNumber ? "card_beer" : "card_back")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: 110, maxHeight: 150)
+                    }
                 }
             }
+            
+            Spacer()
+            
+            if isStart == false {
+                Button {
+                    withAnimation {
+                        isStart = true
+                    }
+                } label: {
+                    Text("게임 시작")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                }
+                .background(Color.black)
+                .cornerRadius(100)
+                .padding()
+            }
+        }
+        .onChange(of: isStart) { _, newValue in
+            if newValue == false { return }
+            
+            guard let randomNumber = (1...userCount).randomElement() else { return }
+            self.targetNumber = randomNumber
         }
     }
 }
@@ -53,26 +86,23 @@ struct GameUserControl: View {
     }
     
     var body: some View {
-        VStack {
-            HStack {
-                userControlButton(isUp: false)
-                
-                Spacer()
-                
-                Text("\(userCount)")
-                    .frame(width: 50)
-                
-                Spacer()
-                
-                userControlButton(isUp: true)
-            }
-            .font(.system(size: 18))
-            .padding()
-            .background(.white)
-            .cornerRadius(400)
+        HStack {
+            userControlButton(isUp: false)
             
             Spacer()
+            
+            Text("\(userCount)")
+                .frame(width: 50)
+            
+            Spacer()
+            
+            userControlButton(isUp: true)
         }
+        .font(.system(size: 18))
+        .padding()
+        .background(.white)
+        .cornerRadius(400)
+        
     }
 }
 
