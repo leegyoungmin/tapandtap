92cd4b0943015aa70c7f3dd47ecaed09290ab5d7
미니 <cow970814@naver.com>
2023-10-06 01:27:17 +0900

    feat : start Repo

---
+++ tapandtap/tapandtapApp.swift
@@ -0,0 +1,17 @@
+//
+//  tapandtapApp.swift
+//  tapandtap
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+        
+
+import SwiftUI
+
+@main
+struct tapandtapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
