e8735ce9d05239b4e5c4b3dd98cc8176e0adfdc3
Kobe <dev.skyachieve91@gmail.com>
2023-10-06 10:00:05 +0900

    :recycle: Refactor: ZStack의 배경색 변경.

--- tapandtap/BalanceView.swift
+++ tapandtap/BalanceView.swift
@@ -9,48 +9,56 @@ import SwiftUI
 
 struct BalanceView: View {
     var body: some View {
-        HStack {
-            VStack(content: {
-                Text("밸런스 게임")
-                    .font(.title)
-                Text("30초 내에 선택하세요~")
-                    .font(.footnote)
-                    .padding(.top, 10)
-                Text("선택하지 못하면 벌주입니다.")
-                    .font(.footnote)
-                    .lineSpacing(20)
+        ZStack {
+            Color.black.ignoresSafeArea()
+            HStack {
                 VStack(content: {
-                    HStack(content: {
-                        Button(action: {
-                            print("A 버튼")
-                        }, label: {
-                            Text("A\n매일 매일 만나기")
-                                .padding(.horizontal, 10)
-                                .frame(width: 100, height: 100)
-                                .foregroundStyle(.white)
-                                .background(.red)
+                    Text("밸런스 게임")
+                        .font(.title)
+                    Text("30초 내에 선택하세요~")
+                        .font(.footnote)
+                        .padding(.top, 10)
+                    Text("선택하지 못하면 벌주입니다.")
+                        .font(.footnote)
+                        .lineSpacing(20)
+                    VStack(content: {
+                        HStack(content: {
+                            Button(action: {
+                                print("A 버튼")
+                            }, label: {
+                                Text("A\n매일 매일 만나기")
+                                    .padding(.horizontal, 10)
+                                    .frame(width: 100, height: 100)
+                                    .foregroundStyle(.white)
+                                    .background(.red)
+                            })
+                            Button(action: {
+                                print("B 버튼")
+                            }, label: {
+                                Text("B\n한달에 한번 만나기")
+                                    .padding(.horizontal, 10)
+                                    .frame(width: 100, height: 100)
+                                    .foregroundStyle(.white)
+                                    .background(.blue)
+                            })
                         })
-                        Button(action: {
-                            print("B 버튼")
-                        }, label: {
-                            Text("B\n한달에 한번 만나기")
-                                .padding(.horizontal, 10)
-                                .frame(width: 100, height: 100)
-                                .foregroundStyle(.white)
-                                .background(.blue)
-                        })
+                        .padding(.top, 20)
                     })
-                    .padding(.top, 20)
+                    
                 })
-            })
-            .frame(width: 300, height: 250, alignment: .center)
+                .frame(width: 300, height: 250, alignment: .center)
+                .background(.white)
+            }
+            .cornerRadius(10)
+            .overlay {
+                RoundedRectangle(cornerRadius: 7)
+                    .stroke(.clear, lineWidth: 1)
+            }
+            .padding([.top, .horizontal])
         }
-        .cornerRadius(10)
-        .overlay {
-            RoundedRectangle(cornerRadius: 7)
-                .stroke(.black, lineWidth: 1)
-        }
-        .padding([.top, .horizontal])
+        
+        
+        
     }
 }
 
