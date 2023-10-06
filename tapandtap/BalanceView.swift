da9db329df1a74aa5ce8573c6974f8f4f1a30474
Kobe <dev.skyachieve91@gmail.com>
2023-10-06 10:38:41 +0900

    :recycle: Refactor: 밸런스 게임 뷰 padding 수정.

--- tapandtap/BalanceView.swift
+++ tapandtap/BalanceView.swift
@@ -10,43 +10,47 @@ import SwiftUI
 struct BalanceView: View {
     var body: some View {
         ZStack {
-            Color.black.ignoresSafeArea()
+            Color(UIColor.systemGray).opacity(0.5).ignoresSafeArea()
             HStack {
                 VStack(content: {
                     Text("밸런스 게임")
                         .font(.title)
+                        .fontWeight(.bold)
                     Text("30초 내에 선택하세요~")
-                        .font(.footnote)
+                        .font(.headline)
+                        .fontWeight(.medium)
                         .padding(.top, 10)
                     Text("선택하지 못하면 벌주입니다.")
-                        .font(.footnote)
+                        .font(.headline)
+                        .fontWeight(.medium)
                         .lineSpacing(20)
-                    VStack(content: {
-                        HStack(content: {
-                            Button(action: {
-                                print("A 버튼")
-                            }, label: {
-                                Text("A\n매일 매일 만나기")
-                                    .padding(.horizontal, 10)
-                                    .frame(width: 100, height: 100)
-                                    .foregroundStyle(.white)
-                                    .background(.red)
-                            })
-                            Button(action: {
-                                print("B 버튼")
-                            }, label: {
-                                Text("B\n한달에 한번 만나기")
-                                    .padding(.horizontal, 10)
-                                    .frame(width: 100, height: 100)
-                                    .foregroundStyle(.white)
-                                    .background(.blue)
-                            })
+                    HStack(content: {
+                        Button(action: {
+                            print("A 버튼")
+                        }, label: {
+                            Text("A\n매일 매일 만나기")
+                                .font(.footnote)
+                                .fontWeight(.bold)
+                                .padding([.horizontal, .top, .bottom], 30)
+                                .frame(maxWidth: .infinity)
+                                .foregroundStyle(.white)
+                                .background(.red)
                         })
-                        .padding(.top, 20)
+                        Button(action: {
+                            print("B 버튼")
+                        }, label: {
+                            Text("B\n한달에 한번 만나기")
+                                .font(.footnote)
+                                .fontWeight(.bold)
+                                .padding([.horizontal, .top, .bottom], 30)
+                                .frame(maxWidth: .infinity)
+                                .foregroundStyle(.white)
+                                .background(.blue)
+                        })
                     })
-                    
+                    .padding(.top, 20)
                 })
-                .frame(width: 300, height: 250, alignment: .center)
+                .padding([.leading, .trailing, .top, .bottom], 50)
                 .background(.white)
             }
             .cornerRadius(10)
@@ -56,9 +60,6 @@ struct BalanceView: View {
             }
             .padding([.top, .horizontal])
         }
-        
-        
-        
     }
 }
 
